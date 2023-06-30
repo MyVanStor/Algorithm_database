c104474ce4441af355cd0ae518b7f0f3f7149f61
Nguyen Van <Vanhoa30903@gmail.com>
2023-07-01 03:27:24 +0700

    Tìm phủ tối thiểu của 1 tập phụ thuộc hàm

--- src/algorithm/FindMinimalCover.java
+++ src/algorithm/FindMinimalCover.java
@@ -1,9 +1,7 @@
 package algorithm;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.ArrayListMultimap;
@@ -20,7 +18,7 @@ public class FindMinimalCover {
 		attributes.add('E');
 		attributes.add('F');
 		attributes.add('G');
-		
+
 		// Tập phụ thuộc hàm ban đầu
 		Multimap<Set<Character>, Set<Character>> functionalDependencies = ArrayListMultimap.create();
 		Set<Character> lhs11 = new HashSet<>();
@@ -29,12 +27,6 @@ public class FindMinimalCover {
 		rhs11.add('B');
 		functionalDependencies.put(lhs11, rhs11);
 
-		Set<Character> lhs15 = new HashSet<>();
-		lhs15.add('A');
-		Set<Character> rhs15 = new HashSet<>();
-		rhs15.add('R');
-		functionalDependencies.put(lhs15, rhs15);
-		
 		Set<Character> lhs12 = new HashSet<>();
 		lhs12.add('A');
 		lhs12.add('B');
@@ -60,56 +52,97 @@ public class FindMinimalCover {
 		rhs14.add('E');
 		rhs14.add('G');
 		functionalDependencies.put(lhs14, rhs14);
-		
+
 		System.out.println("F0 = " + functionalDependencies);
-		Collection<Set<Character>> valueSet = functionalDependencies.get(lhs11);
-		System.out.println(valueSet.iterator().next());
-//		System.out.println("Key = " +findMinimalCover(attributes, functionalDependencies));
+		System.out.println("Key = " + findMinimalCover(attributes, functionalDependencies));
 	}
 
+	/**
+	 * Tìm phủ tối thiểu của 1 tập phụ thuộc hàm
+	 * 
+	 * @param attributes             Tập thuộc tính
+	 * @param functionalDependencies Tập phụ thuộc hàm
+	 * @return Phủ tối thiểu của tập phụ thuộc hàm
+	 */
 	public static Multimap<Set<Character>, Set<Character>> findMinimalCover(Set<Character> attributes,
 			Multimap<Set<Character>, Set<Character>> functionalDependencies) {
-
+		// Phủ tối thiểu cần tìm của tập phụ thuộc hàm
 		Multimap<Set<Character>, Set<Character>> answer = ArrayListMultimap.create();
 
+		// Tập bên trái của tập phụ thuộc hàm
 		Set<Set<Character>> keySets = functionalDependencies.keySet();
+
 		// Bước 1: Biến đổi F về dạng F1 = {Li -> Aj}
-		Map<Set<Character>, Set<Character>> F1 = new HashMap<>();
+		// MultiMap F1 chứa các phụ thuộc của tập F1
+		Multimap<Set<Character>, Set<Character>> F1 = ArrayListMultimap.create();
+		// Duyệt qua từng phần tử bên trái
 		for (Set<Character> key : keySets) {
+			// Lấy về tập value tương ứng với key
 			Collection<Set<Character>> valueKey = functionalDependencies.get(key);
-			for (Set<Character> character : valueKey) {
-				Set<Character> value = new HashSet<>();
-				value.addAll(character);
-				F1.put(key, value);
-			}
-		}
-		System.out.println("F1 = " +F1);
-		// Bước 2: Loại bỏ thuộc tính thừa trong vế trái của các phụ thuộc hàm
-		Multimap<Set<Character>, Set<Character>> F2 = ArrayListMultimap.create();
-		for (Set<Character> key : keySets) {
-			Set<Character> valueKey = F1.get(key);
-			if (key.size() > 1) {
-				for (Character character : key) {
-					Set<Character> keyF2 = new HashSet<>();
-					keyF2.add(character);
-					Set<Character> valueF2 = new HashSet<>(valueKey);
-					if (F1.containsKey(keyF2)) {
-						if (!F1.containsValue(valueF2)) {
-							F2.put(keyF2, valueF2);
-						}
-					} else {
-						F2.put(keyF2, valueF2);
+			// Xét từng value riêng trong tập value
+			for (Set<Character> valueCheck : valueKey) {
+				// Duyệt qua từng thuộc tính trong value
+				for (Character value : valueCheck) {
+					Set<Character> valueInsert = new HashSet<>();
+					valueInsert.add(value);
+					// Check nếu chưa có bộ key - thuộc tính đang xét thì thêm vào F1
+					if (!F1.containsEntry(key, valueInsert)) {
+						F1.put(key, valueInsert);
 					}
 				}
-			} else {
-				F2.put(key, valueKey);
 			}
 		}
+		System.out.println("F1 = " + F1);
+
+		// Bước 2: Loại bỏ thuộc tính thừa trong vế trái của các phụ thuộc hàm
+		// MultiMap F2 chứa các phụ thuộc của F1 sau khi giản ước vế trái
+		Multimap<Set<Character>, Set<Character>> F2 = ArrayListMultimap.create(F1);
+		boolean changed;
+		do {
+			Multimap<Set<Character>, Set<Character>> F21 = ArrayListMultimap.create(F2);
+			changed = false;
+			Set<Set<Character>> keySetF2 = F21.keySet();
+			// Duyệt qua từng phần tử bên trái
+			for (Set<Character> keySet : keySetF2) {
+				if (changed == true)
+					continue;
+				if (keySet.size() > 1) {
+					// Tập các value tương ứng với phần tử key
+					Collection<Set<Character>> valueKeySet = F21.get(keySet);
+					// Xét từng cặp key - value
+					for (Set<Character> valueKey : valueKeySet) {
+						if (changed == true)
+							continue;
+						// Duyệt qua từng thuộc tính của key
+						for (Character character : keySet) {
+							if (changed == true)
+								continue;
+							// Tập các thuộc tính vế trái của phụ thuộc
+							Set<Character> keyCheck = new HashSet<>(keySet);
+							// Loại bỏ thuộc tính đang xét ra khỏi key
+							keyCheck.remove(character);
+							// Tìm bao đóng của key sau khi loại bỏ thuộc tính và kiểm tra có chứa value hay
+							// không
+							if (AttributeClosure.findAttributeClosure(keyCheck, attributes, F21)
+									.containsAll(valueKey)) {
+								changed = true;
+								F2.remove(keySet, valueKey);
+								if (!F2.containsEntry(keyCheck, valueKey)) {
+									F2.put(keyCheck, valueKey);
+								}
+								continue;
+							}
+						}
+					}
+				}
+			}
+		} while (changed);
+
 		System.out.println("F2 = " + F2);
-		
+
 		// Bước 3: Loại bỏ phụ thuộc hàm dư thừa
 		answer = FindNonRedundant.findNonRedundant(attributes, F2);
-		
+
 		return answer;
 	}
 }
